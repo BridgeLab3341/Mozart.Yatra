0f059790d118aa4ada649744a100c60644ed66d0
[BridgeLab3341] <[somashekarvs1996@gmail.com]>
2024-08-27 22:29:02 +0530

    [somu]Add:Implemented Model Classes.

---
+++ Mozart.Yatra/Model/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace Mozart.Yatra.Model
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PhoneNUmber { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ Mozart.Yatra/Model/UserModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Mozart.Yatra.Model
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PhoneNUmber { get; set; }
+        public string City { get; set; }
+    }
+}
